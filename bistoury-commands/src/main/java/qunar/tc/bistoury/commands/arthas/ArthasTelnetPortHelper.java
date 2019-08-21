d13536dfd046e0713802b6f84e57fcff37b48327
wenchao.ren <wenchao.ren@bianlifeng.com>
2019-08-21 14:56:29 +0800

    fix telnet port bug

--- bistoury-commands/src/main/java/qunar/tc/bistoury/commands/arthas/ArthasTelnetPortHelper.java
+++ bistoury-commands/src/main/java/qunar/tc/bistoury/commands/arthas/ArthasTelnetPortHelper.java
@@ -2,6 +2,8 @@ package qunar.tc.bistoury.commands.arthas;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import qunar.tc.bistoury.agent.common.AgentConstants;
 import qunar.tc.bistoury.agent.common.util.AgentUtils;
 import qunar.tc.bistoury.agent.common.util.NetWorkUtils;
@@ -13,6 +15,8 @@ import qunar.tc.bistoury.clientside.common.meta.MetaStores;
  * @since 2019/8/19
  */
 public class ArthasTelnetPortHelper {
+
+    private final static Logger logger = LoggerFactory.getLogger(ArthasTelnetPortHelper.class);
 
     public static int getTelnetPort(String nullableAppCode) {
         if (!AgentUtils.supporGetPidFromProxy()) {
@@ -26,11 +30,13 @@ public class ArthasTelnetPortHelper {
 
         Integer port = appMetaStore.getIntegerProperty(AgentConstants.TELNET_CONNECT_PORT);
         if (port != null) {
+            logger.info("应用[{}]的telnet port为[{}]", nullableAppCode, port);
             return port;
         }
 
         int availablePort = NetWorkUtils.getAvailablePort();
         appMetaStore.put(AgentConstants.TELNET_CONNECT_PORT, String.valueOf(availablePort));
+        logger.info("应用[{}]选择的telnet port为[{}]", nullableAppCode, port);
         return availablePort;
     }
 }
